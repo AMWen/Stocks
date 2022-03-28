52d8d5b1c308de8b3b14cce9f0a50ad30113e93f
Amy Wen <wen.amy@gmail.com>
2022-03-28 13:35:57 -0400

    move user inputs to yml file

--- params.py
+++ params.py
@@ -1,30 +1,33 @@
 import pandas as pd
+import yaml
+from types import SimpleNamespace
 
-# Stock categories
-long_term = ['SLB']  # hold on
-short_term = ['PLTR']  # buy when there's a dip
-buy_asap = ['BAC']
-sell_asap = ['ZM']
 
-# Get owned stocks from CSV file (important columns are ['Quantity', 'Cost Basis Per Share', 'Sell'])
-stocks = pd.read_csv('/Users/amywen/Insight/Stocks/Portfolio_Positions.csv', index_col=False)
+# Load config parameters (primarily desired stocks by categories)
+desired_stocks = yaml.load(open('config/stocks.yml').read(), yaml.FullLoader)  # load from config file
+globals().update(desired_stocks)  # convert dictionary entries into variables within a namespace
 
-# Only keep rows containing stock information, select columns of interest, and convert to float
-stocks = stocks[~stocks['Cost Basis Per Share'].isna()]
-cols = ['Quantity', 'Cost Basis Per Share', 'Sell']
+# Get owned stocks from CSV file (important columns are ['Symbol', 'Quantity', 'Cost Basis Per Share', 'Sell'])
+current_stocks = pd.read_csv(filename, index_col=False)
+
+# Only keep rows containing stock information, clean up cost format, select columns of interest, and convert to float
+stocks = current_stocks[~current_stocks['Cost Basis Per Share'].isna()]
 stocks['Cost Basis Per Share'] = stocks['Cost Basis Per Share'].str.replace('$','').str.replace(',','')
+
+cols = ['Quantity', 'Cost Basis Per Share', 'Sell']
 stocks[cols] = stocks[cols].astype('float')
+
+# Convert the columns into individual lists
 owned_tickers, qty, bought, sell_pct = [list(stocks[col]) for col in ['Symbol'] + cols]
 
-# Want to buy stocks: ticker, target sell price, buy % (if fall X% from target sell price)
-buy_list = [item for item in
-            [('TSLA', 445, 15)]
-            if item[0] not in owned_tickers]
+
+# Stocks to buy (remove if already have it)
+buy_list = [item for item in buy_list if item[0] not in owned_tickers]
 buy_tickers, target, buy_pct = [list(item) for item in zip(*buy_list)]  # make into separate lists
 
-# Stocks to track
-additional = ['INTC']
+# Additional stocks to track
 additional = [stock for stock in set(additional) if stock not in owned_tickers + buy_tickers]  # remove any overlaps
+
 
 # Combine the 3 types
 n_buy = len(buy_tickers)
@@ -33,7 +36,7 @@ bought = bought + [0.0] * (n + n_buy)
 qty = qty + [0.0] * (n + n_buy)
 
 # Calculate sell_prc based on pct specified
-# Mark with a negative if it's a stock I want to buy
-sell_prc = [(1 + sell_pct[i]/100) * bought[i] for i in range(len(owned_tickers))]  # owned stocks
-sell_prc = sell_prc + [-(1 - buy_pct[i]/100) * target[i] for i in range(len(buy_list))] # want to buy stocks
-sell_prc = sell_prc + [10000] * n  # additional stocks
+# Mark with a negative if it's a stock to buy
+sell_prc = [(1 + sell_pct[i]/100) * bought[i] for i in range(len(owned_tickers))]  # owned stocks: sell once profit exceeds specified sell %
+sell_prc = sell_prc + [-(1 - buy_pct[i]/100) * target[i] for i in range(len(buy_list))]  # want to buy stocks: buy once price falls below specified % profit desired
+sell_prc = sell_prc + [10000] * n  # additional stocks: just track so set trigger point to be really high
