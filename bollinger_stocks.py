a06cbbbc6f4030c5d79f1b46f608c04ed9036896
Amy Wen <wen.amy@gmail.com>
2023-06-27 13:11:47 -0400

    allow multiple timeframes simultaneously for bollinger stocks

--- bollinger_stocks.py
+++ bollinger_stocks.py
@@ -6,18 +6,28 @@ from utils import get_bollinger_bands, send_email
 
 # Load config parameters
 desired_stocks = yaml.load(open('config/bollinger_stocks.yml').read(), yaml.FullLoader)  # load from config file
-stocks = desired_stocks['stocks']
-window = desired_stocks['window']
-num_stdev = desired_stocks['num_stdev']
+df_dict = {}
+stocks_to_sell = []
+stocks_to_buy = []
+for stock_type in desired_stocks:
+    stocks = desired_stocks[stock_type]['stocks']
+    window = desired_stocks[stock_type]['window']
+    num_stdev = desired_stocks[stock_type]['num_stdev']
 
-# Get information about the Bollinger Bands and whether buy or sell signals were triggered
-df, stocks_to_sell, stocks_to_buy = get_bollinger_bands(stocks, window, num_stdev)
+    # Get information about the Bollinger Bands and whether buy or sell signals were triggered
+    _df, _stocks_to_sell, _stocks_to_buy = get_bollinger_bands(stocks, window, num_stdev)
 
-# If any rows are identified, send an email
-if len(df) > 0:
+    # If any rows are identified, add to summary
+    if len(_df) > 0:
+        df_dict[stock_type] = _df
+        stocks_to_sell.extend(_stocks_to_sell)
+        stocks_to_buy.extend(_stocks_to_buy)
+
+# If any dfs are identified, send an email
+if len(df_dict) > 0:
     send_email(
         'Bollinger Bands Info',
-        df,
+        df_dict,
         color=['Recommend Buy', 'Recommend Sell', '% Change'],
         bar=['Std Dev Ratio', 'Buy Count', 'Sell Count'],
         other_msg=f"<b>Stocks to buy:</b> {', '.join(stocks_to_buy)}<br>"
